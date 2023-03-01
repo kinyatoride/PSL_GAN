197890ffe8da98881a32336eccbd153eff294862
kinyatoride <kinya.toride@gmail.com>
2023-03-01 11:59:45 -0700

    fix direction

--- hackathon_gan_hp.py
+++ hackathon_gan_hp.py
@@ -5,17 +5,8 @@ import pandas as pd
 import torch
 from torch import nn
 from torch.utils.data import TensorDataset, DataLoader
+import optuna
 torch.manual_seed(0)
-
-# %%
-# Set your parameters
-criterion = nn.BCEWithLogitsLoss()
-n_epochs = 200
-z_dim = 2
-batch_size = 32
-display_step = 500
-lr = 0.00001
-device = 'cuda'
 
 # %% [markdown]
 # # Prepare data
@@ -24,6 +15,8 @@ device = 'cuda'
 df = pd.read_csv('data/Monthly_Average_1950_2009_reservoir.csv')
 
 # %%
+batch_size = 32
+
 tensor_x = torch.Tensor(df.values) # transform to torch tensor
 dataset = TensorDataset(tensor_x) # create your datset
 dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True) # create your dataloader
@@ -36,7 +29,6 @@ dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True) # create y
 def get_generator_block(input_dim, output_dim):
     return nn.Sequential(
         nn.Linear(input_dim, output_dim),
-        nn.Dropout(p=0.25),
         nn.BatchNorm1d(output_dim),
         nn.ReLU(inplace=True),
     )
@@ -117,12 +109,6 @@ class Discriminator(nn.Module):
         '''
         return self.disc
 
-
-gen = Generator(z_dim).to(device)
-gen_opt = torch.optim.Adam(gen.parameters(), lr=lr)
-disc = Discriminator().to(device)
-disc_opt = torch.optim.Adam(disc.parameters(), lr=lr)
-
 # %%
 def get_disc_loss(gen, disc, criterion, real, num_images, z_dim, device):
     '''
@@ -174,118 +160,145 @@ def get_gen_loss(gen, disc, criterion, num_images, z_dim, device):
     return gen_loss
 
 # %% [markdown]
-# # Train
+# # Optimize
 
 # %%
-cur_step = 0
-mean_generator_loss = 0
-mean_discriminator_loss = 0
-test_generator = True # Whether the generator should be tested
-gen_loss = False
-error = False
-for epoch in range(n_epochs):
-    fake__ = []
-    real__ = []
-    # Dataloader returns the batches
-    for real in dataloader:
-        cur_batch_size = len(real[0])
-        real_ = real[0].to(device)
-        # Flatten the batch of real images from the dataset
-        #real = real.view(cur_batch_size, -1).to(device)
+# Set your parameters
+criterion = nn.BCEWithLogitsLoss()
+n_epochs = 200
+z_dim = 2
+display_step = 500
 
-        ### Update discriminator ###
-        # Zero out the gradients before backpropagation
-        disc_opt.zero_grad()
+# lr = 0.00001
+device = 'cpu'
 
-        # Calculate discriminator loss
-        disc_loss = get_disc_loss(gen, disc, criterion, real_, cur_batch_size, z_dim, device)
+# %%
+gen = Generator(z_dim).to(device)
+disc = Discriminator().to(device) 
+test_generator = False # Whether the generator should be tested
 
-        # Update gradients
-        disc_loss.backward(retain_graph=True)
+def objective(trial):
+    
+    # Generate the optimizers.
+    optimizer_name = trial.suggest_categorical("optimizer", ["Adam", "RMSprop", "SGD"])
+    lr = trial.suggest_float("lr", 1e-6, 1e-2, log=True)
+    gen_opt = getattr(torch.optim, optimizer_name)(gen.parameters(), lr=lr)
+    disc_opt = getattr(torch.optim, optimizer_name)(disc.parameters(), lr=lr)
+    
+    # Train the model
+    cur_step = 0
+    mean_generator_loss = 0
+    mean_discriminator_loss = 0
+    for epoch in range(n_epochs):
+        fake__ = []
+        real__ = []
+        for real in dataloader:
+            cur_batch_size = len(real[0])
+            real_ = real[0].to(device)        
+            
+            ### Update discriminator ###
+            # Zero out the gradients before backpropagation
+            disc_opt.zero_grad()
 
-        # Update optimizer
-        disc_opt.step()
+            # Calculate discriminator loss
+            disc_loss = get_disc_loss(gen, disc, criterion, real_, cur_batch_size, z_dim, device)
 
-        # For testing purposes, to keep track of the generator weights
-        if test_generator:
-            old_generator_weights = gen.gen[0][0].weight.detach().clone()
+            # Update gradients
+            disc_loss.backward(retain_graph=True)
 
-        # Update generator
-        gen_opt.zero_grad()
-        gen_loss = get_gen_loss(gen, disc, criterion, cur_batch_size, z_dim, device)
-        gen_loss.backward()
-        gen_opt.step()
+            # Update optimizer
+            disc_opt.step()
+            
+            # For testing purposes, to keep track of the generator weights
+            if test_generator:
+                old_generator_weights = gen.gen[0][0].weight.detach().clone()
 
-        # For testing purposes, to check that your code changes the generator weights
-        if test_generator:
-            try:
-                assert lr > 0.0000002 or (gen.gen[0][0].weight.grad.abs().max() < 0.0005 and epoch == 0)
-                assert torch.any(gen.gen[0][0].weight.detach().clone() != old_generator_weights)
-            except:
-                error = True
-                print("Runtime tests have failed")
+            # Update generator
+            gen_opt.zero_grad()
+            gen_loss = get_gen_loss(gen, disc, criterion, cur_batch_size, z_dim, device)
+            gen_loss.backward()
+            gen_opt.step()
 
-        # Keep track of the average discriminator loss
-        mean_discriminator_loss += disc_loss.item() / display_step
+            # For testing purposes, to check that your code changes the generator weights
+            if test_generator:
+                try:
+                    assert lr > 0.0000002 or (gen.gen[0][0].weight.grad.abs().max() < 0.0005 and epoch == 0)
+                    assert torch.any(gen.gen[0][0].weight.detach().clone() != old_generator_weights)
+                except:
+                    error = True
+                    print("Runtime tests have failed")
+                    
+            # Keep track of the average discriminator loss
+            mean_discriminator_loss += disc_loss.item() / display_step
 
-        # Keep track of the average generator loss
-        mean_generator_loss += gen_loss.item() / display_step
-        real__.append(real_.cpu().detach().numpy())
-        fake_noise = get_noise(cur_batch_size, z_dim, device=device)
-        fake__.append(gen(fake_noise).cpu().detach().numpy())
-        
-        ### Visualization code ###
-        if cur_step % display_step == 0 and cur_step > 0:
-            print(f"Step {cur_step}: Generator loss: {mean_generator_loss}, discriminator loss: {mean_discriminator_loss}")
+            # Keep track of the average generator loss
+            mean_generator_loss += gen_loss.item() / display_step
+            real__.append(real_.cpu().detach().numpy())
             fake_noise = get_noise(cur_batch_size, z_dim, device=device)
-            fake = gen(fake_noise)
-            # show_tensor_images(fake)
-            # show_tensor_images(real)
-            mean_generator_loss = 0
-            mean_discriminator_loss = 0
-        cur_step += 1
+            fake__.append(gen(fake_noise).cpu().detach().numpy())
+            
+            ### Visualization code ###
+            if cur_step % display_step == 0 and cur_step > 0:
+                print(f"Step {cur_step}: Generator loss: {mean_generator_loss}, discriminator loss: {mean_discriminator_loss}")
+                fake_noise = get_noise(cur_batch_size, z_dim, device=device)
+                fake = gen(fake_noise)
+                # show_tensor_images(fake)
+                # show_tensor_images(real)
+                mean_generator_loss = 0
+                mean_discriminator_loss = 0
+            cur_step += 1
 
-# Evaluate the model
-fake_noise = get_noise(720, z_dim, device=device)
-fake = gen(fake_noise).cpu().detach().numpy()
-real = df.values
+        # Evaluate the model
+        fake_noise = get_noise(720, z_dim, device=device)
+        fake = gen(fake_noise).cpu().detach().numpy()
+        real = df.values
+        
+        # mean error (%)
+        mean_error = (fake.mean(axis=0) - real.mean(axis=0)) / real.mean(axis=0) * 100
+        mean_mape = np.abs(mean_error).mean()
+        
+        # Calculate empirical CDFs
+        bins = np.linspace(0, 15, 61)   # Maximum 15 mm for now
+        binsize = bins[1:] - bins[:-1]
 
-# mean error (%)
-mean_error = (fake.mean(axis=0) - real.mean(axis=0)) / real.mean(axis=0) * 100
+        real_hist = np.apply_along_axis(lambda a: np.histogram(a, bins=bins, density=True)[0], 0, real)
+        real_ecdf = np.cumsum(real_hist, axis=0) * binsize[:, np.newaxis]
 
-# std error (%)
-std_error = (fake.std(axis=0) - real.std(axis=0)) / real.std(axis=0) * 100
+        fake_hist = np.apply_along_axis(lambda a: np.histogram(a, bins=bins, density=True)[0], 0, fake)
+        fake_ecdf = np.cumsum(fake_hist, axis=0) * binsize[:, np.newaxis]
 
-# Combine
-stats_df = pd.DataFrame([mean_error, std_error],
-                        columns=df.columns,
-                        index=['Mean error', 'Std error'])
+        # Continuous ranked probability score (CRPS)
+        crps = np.sum(np.abs((real_ecdf - fake_ecdf) * binsize[:, np.newaxis]), axis=0)
+        crps_mean = crps.mean()
+                          
+    return crps_mean
 
-# Mean absolute percent error
-mape_df = stats_df.abs().mean(axis=1)
+# %%
+study = optuna.create_study(direction='maximize')
+study.optimize(objective, n_trials=3)
 
-with pd.option_context('display.float_format', '{:.2f}%'.format, 'display.expand_frame_repr', False):
-    print('---Individual watersheds---')
-    print(stats_df)
-    print('---Summary (MAPE)---')
-    print(mape_df)
+# %% [markdown]
+# # Check results
 
-# Calculate empirical CDFs
-bins = np.linspace(0, 15, 31)   # Maximum 15 mm for now
-binsize = bins[1:] - bins[:-1]
+# %%
+pruned_trials = study.get_trials(deepcopy=False, states=[optuna.trial.TrialState.PRUNED])
+complete_trials = study.get_trials(deepcopy=False, states=[optuna.trial.TrialState.COMPLETE])
 
-real_hist = np.apply_along_axis(lambda a: np.histogram(a, bins=bins, density=True)[0], 0, real)
-real_ecdf = np.cumsum(real_hist, axis=0) * binsize[:, np.newaxis]
+print("Study statistics: ")
+print("  Number of finished trials: ", len(study.trials))
+print("  Number of pruned trials: ", len(pruned_trials))
+print("  Number of complete trials: ", len(complete_trials))
 
-fake_hist = np.apply_along_axis(lambda a: np.histogram(a, bins=bins, density=True)[0], 0, fake)
-fake_ecdf = np.cumsum(fake_hist, axis=0) * binsize[:, np.newaxis]
+print("Best trial:")
+trial = study.best_trial
 
-# Continuous ranked probability score (CRPS)
-crps = np.sum(np.abs((real_ecdf - fake_ecdf) * binsize[:, np.newaxis]), axis=0)
-crps_df = pd.DataFrame([crps], columns=df.columns, index=['CRPS'])
+print("  Value: ", trial.value)
 
-with pd.option_context('display.float_format', '{:.2f}'.format, 'display.expand_frame_repr', False):
-    print('---Individual watersheds---')
-    print(crps_df)
-    print('---Mean CRPS---')
-    print(f'{crps.mean():.2f}')
+print("  Params: ")
+for key, value in trial.params.items():
+    print("    {}: {}".format(key, value))
+
+# %%
+
+
+
